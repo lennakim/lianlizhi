bd7d9b958bc6270bb0121b38b9dd6b71ab8a3585
shooter <shooter.hero@gmail.com>
2018-08-15 02:04:46 +0800

    模板遍历包包

--- pages/admin/list/box.js
+++ pages/admin/list/box.js
@@ -1,11 +1,67 @@
+var boxData = [
+  {
+    "title": "克洛伊",
+    "price": "2300",
+    "img": "/images/chloe.jpg",
+    "desc": "米白色女士手提双肩包",
+    "state": "上架"
+  },
+  {
+    "title": "香奈儿",
+    "price": "6000",
+    "img": "/images/lv.jpg",
+    "desc": "香奈儿焦糖色牛皮女士单肩斜挎包",
+    "state": "上架"
+  },
+  {
+    "title": "迪奥",
+    "price": "6832",
+    "img": "/images/dior.jpg",
+    "desc": "迷你牛皮斜挎包",
+    "state": "上架"
+  }
+]
+
+
+var checkoutBox = [
+  {
+    "title": "香奈儿",
+    "price": "6000",
+    "img": "/images/lv.jpg",
+    "desc": "香奈儿焦糖色牛皮女士单肩斜挎包",
+    "state": "已出售"
+  },
+  {
+    "title": "克洛伊",
+    "price": "2300",
+    "img": "/images/chloe.jpg",
+    "desc": "米白色女士手提双肩包",
+    "state": "已出售"
+  }
+]
+
+
+var expireBox = [
+  {
+    "title": "克洛伊",
+    "price": "2300",
+    "img": "/images/chloe.jpg",
+    "desc": "米白色女士手提双肩包",
+    "state": "过期"
+  }
+]
+
 const sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
   data: {
     tabs: ["寄卖列表", "结账列表", "到期列表"],
-    activeIndex: 1,
+    activeIndex: 1, // 显示哪个 navbar
     sliderOffset: 0,
-    sliderLeft: 0
+    sliderLeft: 0,
+    boxs: boxData,
+    checkouts: checkoutBox,
+    expires: expireBox,
   },
   onLoad: function () {
     var self = this;
