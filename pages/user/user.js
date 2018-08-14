2eb95e9bd290334cafdd5dbcf74ab73190c2d8fa
shooter <shooter.hero@gmail.com>
2018-08-14 12:30:56 +0800

    用户

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,1 +1,26 @@
-Page({})
+Page({
+
+  data: {
+    checkout: ['微信', '支付宝', '银行转账']
+  },
+
+  bindPickerChange: function (e) {
+    console.log('picker 数值是', e.detail.value)
+
+    this.setData({
+      inx: e.detail.value
+    })
+  },
+
+  enterAdmin: function () {
+    wx.navigateTo({
+      url: '/pages/admin/login'
+
+    })
+
+    wx.setNavigationBarTitle({
+      title: '后台'
+    })
+    
+  }
+})
