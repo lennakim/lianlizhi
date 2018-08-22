ddebe10f17ca978b3dddc0b092aebcfd951b6640
shooter <shooter.hero@gmail.com>
2018-08-22 19:55:26 +0800

    七牛云上传

--- pages/admin/box/new.js
+++ pages/admin/box/new.js
@@ -1,87 +1,61 @@
-// pages/admin/box/new.js
+const qiniuUploader = require("../../../utils/qiniuUploader");
+
+function initQiniu() {
+  var options = {
+    region: 'SCN',
+    uptokenFunc: function() {
+      return ""
+    },
+    domain: 'http://pdum902ru.bkt.clouddn.com/',
+    shouldUseQiniuFileName: false
+  };
+
+  qiniuUploader.init(options);
+}
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-    files: []
+    imgObj: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function() {
 
   },
-
   chooseImage: function(e) {
+    initQiniu();
+
     var that = this;
     wx.chooseImage({
-      count: 9, // 默认9
+      count: 1, // 默认9
 
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function(res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        that.setData({
-          files: that.data.files.concat(res.tempFilePaths)
-        });
+        var filePath = res.tempFilePaths[0];
+
+        qiniuUploader.upload(filePath, function(res) {
+          that.setData({
+            'imgObj': res // res.imageURL
+          });
+        })
+      },
+      error: function(err) {
+        console.log('error: ' + JSON.stringify(err));
       }
     })
   },
   previewImage: function(e) {
     wx.previewImage({
       current: e.currentTarget.id, // 当前显示图片的http链接
-      urls: this.data.files // 需要预览的图片http链接列表
+      urls: this.imgObj.imageURL // 需要预览的图片http链接列表
     })
   }
 })
